1bc603213669d98bcafa85bcfb8034689451a61d
Matias Pedelhez <matiaspedelhezinf@gmail.com>
2022-11-10 22:51:24 -0300

    diff naming approach for loggings

--- run.bat
+++ run.bat
@@ -12,6 +12,8 @@ echo BATCH file init > %LOGFILE%
 python --version 2 > nul
 if errorlevel 1 goto NOPYTHON
 
+if not exist %~dp0mouse_yoke.py goto NOSCRIPT
+
 echo Trying to update pip... >> %LOGFILE%
 python -m pip install --upgrade pip >> %LOGFILE%
 
@@ -24,6 +26,11 @@ python ./mouse_yoke.py %DATE_TIME%.log
 exit /b 0
 
 :NOPYTHON
-echo Error^: Python is not installed or it's not reachable. If you believe you have Python installed, check your PATH settings.
-echo FATAL: PYTHON IS NOT INSTALLED >> %LOGFILE%
+echo Error^: Python is not installed or is not reachable. If you believe you have Python installed, check your PATH settings.
+echo FATAL: Python is not installed. >> %LOGFILE%
+pause
+
+:NOSCRIPT
+echo Error^: mouse_yoke.py is not reachable. Is it in the same path as run.bat?
+echo FATAL: mouse_yoke.py is not reachable. >> %LOGFILE%
 pause
