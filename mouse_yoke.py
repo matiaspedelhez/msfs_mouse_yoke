c7d87be4b06839beb0a4a53db7c4844c7afc2ff7
Matias Pedelhez <matiaspedelhezinf@gmail.com>
2023-03-25 18:06:49 -0300

    refactoring code to class

--- mouse_yoke.py
+++ mouse_yoke.py
@@ -1,99 +1,99 @@
 from pyautogui import size, moveTo
 from pynput import mouse, keyboard
-from reprint import output
 from threading import Thread
-import vgamepad as vg
 import logging
 import sys
 import json
 import time
 
 
-with open("./config.json") as config_file:
-    configs = json.load(config_file)
+# logging.basicConfig(filename=f"./logs/{sys.argv[1]}", format="%(asctime)s - %(message)s")
 
+class MouseController:
+    def __init__(self, gamepad, status="Stopped"):
+        self.status = "Stopped" # "Stopped" or "Paused" or "Running"
+        self.gamepad = gamepad
+        self.raw_position_x = None
+        self.raw_position_y = None
+        self.transformed_position_x = 0
+        self.transformed_position_y = 0
+        self.transformed_scrollwheel = 0
+        self.screen_size = {"width": None, "height": None}
+        self.mouse_instance = None
+        self.keyboard_instance = None
+        
 
-logging.basicConfig(filename=f"./logs/{sys.argv[1]}", format="%(asctime)s - %(message)s")
-gamepad = vg.VX360Gamepad()
-screen_size = size()
-
-currentThrottleStep = 0
-pixelsToFloatX = 0.0
-pixelsToFloatY = 0.0
-global_x = 0
-global_y = 0
-active = False
-
-
-def mouseYoke(x, y):
-    global pixelsToFloatX, pixelsToFloatY
-    global global_x, global_y
-
-    global_x = x
-    global_y = y
+    def __loop(self, mouseInput, keysInput):
+        # creating instance of pynput
+        movement, scroll = mouseInput
+        
+        self.mouse_instance = mouse.Listener(on_move=movement, on_scroll=scroll)
+        self.keyboard_instance = keyboard.Listener(on_release=keysInput)
     
-    if active:
-        if x >= 0 and x <= screen_size.width:
-            pixelsToFloatX = x / (screen_size.width / 2) - 1
-        if y >= 0 and y <= screen_size.height:
-            pixelsToFloatY = y / (screen_size.height / 2) - 1
+        self.mouse_instance.start()
+        self.keyboard_instance.start()
 
-        gamepad.left_joystick_float(x_value_float=pixelsToFloatX, y_value_float=pixelsToFloatY)
-        gamepad.update()
+    def __stopLoop(self):
+        # deleting instance of pynput
+        del self.mouse_instance
+        del self.keyboard_instance
 
+    def __mouseMovementCallback(self):
+        if self.status == "Running":
+            self.__transformRawPositions()
 
-def throttle(x, y, dx, dy):
-    global currentThrottleStep
-     
-    if active:
-        if currentThrottleStep < configs['throttle_sensitivity'] and dy == 1:
-            currentThrottleStep += dy
-        if currentThrottleStep > 0 and dy == -1:
-            currentThrottleStep += dy
+            self.gamepad.left_joystick_float(x_value_float=self.transformed_position_x, y_value_float=self.transformed_position_y)
+            self.gamepad.update()
+
+    def __scrollwheelCallback(self):
+        pass
+
+    def __getScreenSize(self):
+        # get width and height of screen (includes multiple monitors)
+        self.screen_size["width"], self.screen_size["height"] = size()
+
+    def __transformRawPositions(self, scrollSensitivity, dy):
+        # transform pointer cordinates into -1 to 1 floatable values 
+        self.__getScreenSize()
+
+        # transform mouse value
+        if self.raw_position_x >= 0 and self.raw_position_x  <= self.screen_size.width:
+            self.transformed_position_x = self.raw_position_x / (self.screen_size.width / 2) - 1
+        if self.raw_position_y >= 0 and self.raw_position_y <= self.screen_size.height:
+            self.transformed_position_y = self.raw_position_y / (self.screen_size.height / 2) - 1
         
-        stepsToFloat = currentThrottleStep / (configs['throttle_sensitivity'] / 2) - 1
-
-        gamepad.right_joystick_float(x_value_float=stepsToFloat, y_value_float=0)
-        gamepad.update()
-
-
-def onKeyRelease(key):
-    global active
-
-    if key == keyboard.KeyCode.from_char(configs["master_key"]):
-        active = not active
-        if active: moveTo(screen_size.width / 2, screen_size.height / 2)
+        # transform scrollwheel value
+        if self.transformed_scrollwheel < scrollSensitivity and dy == 1:
+            self.transformed_scrollwheel += dy
+        if self.transformed_scrollwheel > 0 and dy == -1:
+            self.transformed_scrollwheel -= dy
 
 
-def userInterface():
-    with output(initial_len=8, interval=0) as output_lines:
-        while True:
-
-            output_lines[0] = f"+{' Status: ' + ('ACTIVE' if active else 'INACTIVE') + ' ':—^60}+"
-            output_lines[1] = f"|{'':^60}|"
-            output_lines[2] = f"|{'Axis':^20}{'Raw input':^20}{'Conversion':^20}|"
-            output_lines[3] = f"+{'':—^60}+"
-            output_lines[4] = f"|{'X':^20}{global_x:^20}{'{:.2f}'.format((pixelsToFloatX + 1) * 50) + '%':^20}|"
-            output_lines[5] = f"|{'Y':^20}{global_y:^20}{'{:.2f}'.format((pixelsToFloatY + 1) * 50) + '%':^20}|"
-            output_lines[6] = f"|{'THROTTLE':^20}{currentThrottleStep:^20}{'{:.2f}'.format((currentThrottleStep * 100 / configs['throttle_sensitivity'])) + '%':^20}|"
-            output_lines[7] = f"+{'':—^60}+"
-
-            time.sleep(0.05)
+    def run(self, screen_size_x, screen_size_y):
+        if self.status == "Running":
+            logging.warning("Cannot start MouseController: it is already running.")
+            return
+        else:
+            self.status = "Running"
+            logging.info("MouseController is now running.")
 
 
-if __name__ == "__main__":
-    logging.warning("mouse_yoke.py is now running\n\n")
+    def stop(self):
+        if self.status == "Stopped":
+            logging.warning("Cannot stop MouseController: it is already stopped.")
+            return
+        else:
+            self.status = "Stopped"
+            logging.info("MouseController is now stopped.")
 
-    try:
-        ui = Thread(target=userInterface)
-        ms = mouse.Listener(on_move=mouseYoke, on_scroll=throttle)
-        kb = keyboard.Listener(on_release=onKeyRelease)
-        
-        ms.start()
-        kb.start()
-        ui.start()
-        ms.join()
-        kb.join()
 
-    except Exception as e:
-        logging.critical("Exception occurred", exc_info=True)
+    def pause(self):
+        if self.status == "Paused":
+            logging.warning("Cannot pause MouseController: it is already paused.")
+            return
+        else:
+            self.status = "Paused"
+            logging.info("MouseController is now paused.")    
+
+mouse_controller = MouseController #debug
+mouse_controller.run(1920, 1080) #debug
