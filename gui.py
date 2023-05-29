12e624cc589df2a3016ca3522000b11d5c60d2e3
Matias Pedelhez <matiaspedelhezinf@gmail.com>
2023-05-29 02:18:48 -0300

    gui init

---
+++ gui.py
@@ -0,0 +1,13 @@
+import tkinter, customtkinter
+
+# settings
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+# app frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("MouseYoke MOD by @matiaspedelhez")
+
+# ui elements
+status = customtkinter.CTkLabel(app, text="Running")
